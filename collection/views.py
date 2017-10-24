302ad4079e79322d082a7191d41ad5ec8a2256e0
Brian McMahon <cipher813@gmail.com>
2017-10-24 09:15:07 +0630

    posts are now editable

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from collection.forms import ThingForm
 from collection.models import Thing
 
 # Create your views here.
@@ -13,3 +14,26 @@ def thing_detail(request, slug):
     return render(request, 'things/thing_detail.html', {
         'thing': thing,
 })
+
+def edit_thing(request, slug):
+    # grab the object
+    thing = Thing.objects.get(slug=slug)
+    # set the form
+    form_class = ThingForm
+
+    # if coming to view from a submitted form
+    if request.method == 'POST':
+        # grab data from submitted form and apply to form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    # otherwise just create the form
+    else:
+        form = form_class(instance=thing)
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+        'thing': thing,
+        'form': form,
+    })
